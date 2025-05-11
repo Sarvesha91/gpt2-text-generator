523f0bb808d6e372c42629071f1bc2f6d0389e53
Sarvesha Sinha <112442687+Sarvesha91@users.noreply.github.com>
2025-05-11 16:37:53 +0530

    Update get_model.py

--- get_model.py
+++ get_model.py
@@ -3,8 +3,8 @@
 # Downloads model(s)
 # from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-# GPT2LMHeadModel.from_pretrained('distilgpt2').save_pretrained('./src/distilgpt2')
-# GPT2Tokenizer.from_pretrained('distilgpt2').save_pretrained('./src/distilgpt2')
+# GPT2LMHeadModel.from_pretrained('distilgpt2').save_pretrained('distilgpt2')
+# GPT2Tokenizer.from_pretrained('distilgpt2').save_pretrained('distilgpt2')
 
 from transformers import AutoTokenizer, AutoModelWithLMHead
 
